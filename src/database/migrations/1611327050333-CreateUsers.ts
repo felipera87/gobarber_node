c5f25c4cb90ff9c2438834d6886eb03d0677547c
felipe_ra87@hotmail.com <felipe.reis@interlinkbrasil.com>
2021-01-22 14:02:25 -0300

    Refactored model Appointment to include it's user reference (provider_id)

--- src/database/migrations/1611327050333-CreateUsers.ts
+++ src/database/migrations/1611327050333-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1611327050333 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
